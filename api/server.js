ad35e184701c7c63a8410d4da42b3543b103bc18
Paul Pessoa <74559558+paulpessoa@users.noreply.github.com>
2023-07-13 15:38:46 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,19 +1,44 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+server.use(middlewares);
 
-// Export the Server API
-module.exports = server
+// Adicione a seguinte linha para permitir o uso de variáveis de ambiente na Vercel
+// const apiKey = process.env.API_KEY;
+const apiKey = "123456"
+
+// Middleware para verificar a chave da API
+server.use((req, res, next) => {
+  const { headers } = req;
+  const requestApiKey = headers['x-api-key'];
+
+  if (requestApiKey && requestApiKey === apiKey) {
+    next();
+  } else {
+    res.status(401).json({ error: 'API key is required or invalid.' });
+  }
+});
+
+// Adicione as rotas personalizadas antes do uso do router
+server.get('/api/users', (req, res) => {
+  const users = router.db.get('users').value();
+  res.json(users);
+});
+
+server.get('/api/threads', (req, res) => {
+  const threads = router.db.get('threads').value();
+  res.json(threads);
+});
+
+server.use(router);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
+
+// Exporte a API do servidor
+module.exports = server;
