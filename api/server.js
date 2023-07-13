6e6d9ca47a8e36314f4c7a64544ac562644e26a8
Paul Pessoa <74559558+paulpessoa@users.noreply.github.com>
2023-07-13 16:10:40 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,11 +1,8 @@
+// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server');
-const cors = require('cors');
-
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-
-server.use(cors()); // Habilita o CORS para todas as origens
 
 server.use(middlewares);
 
@@ -36,9 +33,12 @@ server.get('/api/threads', (req, res) => {
   res.json(threads);
 });
 
+server.use(router);
+
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
 
+// Exporte a API do servidor
 module.exports = server;
